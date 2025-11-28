b762fa46cd483eef7a61d2dbbafe95e35dd2a4b3
tchenavier <thibaud.chenavier@gmail.com>
2025-11-28 12:21:07 +0100

    bas niveau fonction et boucle

---
+++ Oceant.js
@@ -0,0 +1,37 @@
+//alert ("Bonjour maître, comment allez vous ?");
+let death = "Tout";
+let type = "poisson";
+
+function aufWiedersehen()
+{
+    console.log("Tschüss");
+    console.log("auf Wiedersehen");
+}
+function skellete() {
+    let mort = 250;
+    return mort;
+}
+
+for (let i = 0; i < 25; i++) //La premier et effectuer une fois au début de la boucle (une seul chose) ; expresion/condition pour que la boucle continue; et une expresion est effectuer a chaque fin de boucle (ou une fonction pour avoir plusieur chose qui sexécute)
+{
+    console.log(death + " les " + type)
+    /*while (i < 5) { //danger
+
+    }*/
+}
+let nombre = skellete();
+console.log("Il y a "+ nombre + " skellete !");
+aufWiedersehen();
+
+console.log("Il y aura peut être un tableu")
+let tab = [];
+tab.push ("cadavre");
+tab.push(250);
+console.log("Dans la mer il y a")
+console.log(tab);
+let tabDeux = [24,"pencton",function(){"je suis une fonction";}]
+console.log(tabDeux[0])
+console.log(tabDeux[2])
+tabDeux.forEach(element =>{ //pour parcourire tout le tableau
+    console.log(element); //affiche les élément récupéré
+});
---
+++ rivier.js
@@ -0,0 +1,23 @@
+var c4 = "Nitrogliserine"; //disponible dans tout le programe
+var eau = "H2o";
+let terre = "terre" // permmet de declared une variable que pour une fonction, se détruit après
+const TERRE_O = 8;// C'est une constante quoi
+const TERRE_H = 1;
+const idorphobe = false;
+
+console.log(eau);
+console.log(TERRE_H);
+console.log(TERRE_O);
+console.log("Poids");
+console.log(TERRE_O * 2 + TERRE_H);
+console.log("La rivier coule toujour !");
+console.log(window)
+console.log(c4)
+c4 = "Nitro"
+console.log(c4)
+function Eau(parametre)
+{
+    console.log(parametre+"eau");
+}
+Eau("verss")
+Eau("feu et ")
